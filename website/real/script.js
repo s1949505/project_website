af15e9d579fdbe7e98d68326765d2b8752cd475e
Calvin <s1949505@ed.ac.uk>
2024-01-23 17:49:01 +0000

    feeding information from the forms to the complete page working. Need to format it

--- website/real/script.js
+++ website/real/script.js
@@ -1,97 +1,140 @@
-// common.js// index.js
-function saveText(nextPage) {
+
+function saveText1(nextPage) {
+
     // Get the text from the textarea
-    var DatasetName = document.getElementById("myTextarea").value;
+    const DatasetName = document.getElementById("DatasetName").value;
 // Save the text to localStorage (you can use other storage mechanisms)
     localStorage.setItem("DatasetName", DatasetName);
     
-    var description = document.getElementById("description").value;
+    const description = document.getElementById("description").value;
     localStorage.setItem("description", description);
 
-    var motivation = document.getElementById("motivation").value;
+    const motivation = document.getElementById("motivation").value;
     localStorage.setItem("motivation", motivation);
 
-    var research = document.getElementById("research").value;
+    const accessibilityOption = document.querySelector('input[name="accessibility"]:checked');
+    const accessibility = accessibilityOption ? accessibilityOption.value : null;
+    localStorage.setItem("accessibility", accessibility);
+
+    const research = document.getElementById("research").value;
     localStorage.setItem("research", research);
 
-    var authors = document.getElementById("authors").value;
+    const authors = document.getElementById("authors").value;
     localStorage.setItem("authors", authors);
 
-    var contributors = document.getElementById("contributors").value;
+    const contributors = document.getElementById("contributors").value;
     localStorage.setItem("contributors", contributors);
 //DO RADIO QUESTIONS
-    var date = document.getElementById("date").value;
+
+    const fundingOption = document.querySelector('input[name="funding"]:checked');
+    const funding = fundingOption ? fundingOption.value : null;
+    localStorage.setItem("funding", funding);
+
+    const combinationOption = document.querySelector('input[name="combination"]:checked');
+    const combination = combinationOption ? combinationOption.value : null;
+    localStorage.setItem("combination", combination);
+
+    const date = document.getElementById("date").value;
     localStorage.setItem("date", date);
 
-    var version = document.getElementById("version").value;
+    const version = document.getElementById("version").value;
     localStorage.setItem("version", version);
 
-    var applications = document.getElementById("applications").value;
-    localStorage.setItem("applications", applications);
+    window.location.href = nextPage;
 
-    var datatypes = document.getElementById("datatypes").value;
-    localStorage.setItem("datatypes", datatypes);
-
-    var qorq = document.getElementById("qorq").value;
-    localStorage.setItem("qorq", qorq);
-
-    var size = document.getElementById("size").value;
-    localStorage.setItem("size", size);
-
-    var personal = document.getElementById("personal").value;
-    localStorage.setItem("personal", personal);
-
-    var flaws = document.getElementById("flaws").value;
-    localStorage.setItem("flaws", flaws);
-
-    var splits = document.getElementById("splits").value;
-    localStorage.setItem("splits", splits);
-
-    var collection = document.getElementById("collection").value;
-    localStorage.setItem("collection", collection);
-
-    var format = document.getElementById("format").value;
-    localStorage.setItem("format", format);
-
-    var languages = document.getElementById("languages").value;
-    localStorage.setItem("languages", languages);
-
-    var doi = document.getElementById("doi").value;
-    localStorage.setItem("doi", doi);
-
-    var licence = document.getElementById("licence").value;
-    localStorage.setItem("licence", licence);
-
-    var update = document.getElementById("update").value;
-    localStorage.setItem("update", update);
-
-    var Uses = document.getElementById("Uses").value;
-    localStorage.setItem("Uses", Uses);
-
-    var unsafe = document.getElementById("unsafe").value;
-    localStorage.setItem("unsafe", unsafe);
-
-    var bias = document.getElementById("bias").value;
-    localStorage.setItem("bias", bias);
-
-    var cite = document.getElementById("cite").value;
-    localStorage.setItem("cite", cite);
-
-    var further = document.getElementById("further").value;
-    localStorage.setItem("further", further);
-
-    if (nextPage){
-        window.location.href = nextPage.html
-    }
 
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    // Retrieve text from localStorage (you can use other storage mechanisms)
-    var cite = localStorage.getItem("cite");
+function saveText2(nextPage) {
 
-    // Display the text in the paragraph
-    if (savedText) {
-        document.getElementById("displayParagraph").innerText = cite;
+    const applications = document.getElementById("applications").value;
+    localStorage.setItem("applications", applications);
+
+    const datatypes = document.getElementById("datatypes").value;
+    localStorage.setItem("datatypes", datatypes);
+
+    const qorqOption = document.querySelector('input[name="qorq"]:checked');
+    const qorq = qorqOption ? qorqOption.value : null;
+    localStorage.setItem("qorq", qorq);
+
+    const size = document.getElementById("size").value;
+    localStorage.setItem("size", size);
+
+    const personalOption = document.querySelector('input[name="personal"]:checked');
+    const personal = personalOption ? personalOption.value : null;
+    localStorage.setItem("personal", personal);
+
+    const flaws = document.getElementById("flaws").value;
+    localStorage.setItem("flaws", flaws);
+
+    const splits = document.getElementById("splits").value;
+    localStorage.setItem("splits", splits);
+
+    const annotationOption = document.querySelector('input[name="annotation"]:checked');
+    const annotation = annotationOption ? annotationOption.value : null;
+    localStorage.setItem("annotation", annotation);
+
+    const collection = document.getElementById("collection").value;
+    localStorage.setItem("collection", collection);
+
+    const format = document.getElementById("format").value;
+    localStorage.setItem("format", format);
+
+    const languages = document.getElementById("languages").value;
+    localStorage.setItem("languages", languages);
+
+    const doi = document.getElementById("doi").value;
+    localStorage.setItem("doi", doi);
+
+    const licence = document.getElementById("licence").value;
+    localStorage.setItem("licence", licence);
+
+    const update = document.getElementById("update").value;
+    localStorage.setItem("update", update);
+
+    window.location.href = nextPage;
+
+
+}
+
+function saveText3(nextPage) {
+
+    const maintainedOption = document.querySelector('input[name="maintained"]:checked');
+    const maintained = maintainedOption ? maintainedOption.value : null;
+    localStorage.setItem("maintained", maintained);
+
+    const Uses = document.getElementById("Uses").value;
+    localStorage.setItem("Uses", Uses);
+
+    const unsafe = document.getElementById("unsafe").value;
+    localStorage.setItem("unsafe", unsafe);
+
+    const bias = document.getElementById("bias").value;
+    localStorage.setItem("bias", bias);
+
+    const cite = document.getElementById("cite").value;
+    localStorage.setItem("cite", cite);
+
+    const further = document.getElementById("further").value;
+    localStorage.setItem("further", further);
+
+    window.location.href = nextPage;
+
+}
+
+// displayData
+document.addEventListener("DOMContentLoaded", function () {
+    var displayContainer = document.getElementById('displayContainer');
+
+    // Iterate through localStorage keys
+    for (var i = 0; i < localStorage.length; i++) {
+        var key = localStorage.key(i);
+        var storedData = localStorage.getItem(key);
+
+        // Display the key-value pair on the webpage
+        var content = "<p>" + key + ": " + storedData + "</p>";
+        displayContainer.innerHTML += content;
+    
     }
 });
+
